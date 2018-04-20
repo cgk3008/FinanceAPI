f8647eb5537e5cb9fc061dd7e49d58466eeb9dbb
cgk3008 <36110991+cgk3008@users.noreply.github.com>
2018-04-20 12:21:12 -0400

    API setup, added classes from FinanceApp "Finance.cs", added controller with calls to database queries located in the Identity model.

---
+++ HouseFinanceAPI/Controllers/FinancesAPIController.cs
@@ -0,0 +1,97 @@
+﻿using HouseFinanceAPI.Models;
+using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Threading.Tasks;
+using System.Web.Http;
+using static HouseFinanceAPI.Models.FinanceAPI;
+
+namespace HouseFinanceAPI.Controllers
+{
+    [RoutePrefix("api/Finance")]
+    public class FinancesAPIController : ApiController
+    {
+        private ApplicationDbContext db = new ApplicationDbContext();
+
+        //[Route("Budget")]
+        //public async Task<List<string>> GetBudget()
+        //{
+        //    return await db.GetBudget();
+        //}
+        [Route("GetAccounts")]
+        public async Task<List<PersonalAccount>> GetAccounts(int hhId)
+        {
+            return await db.GetAccounts(hhId);
+        }
+
+
+
+        //[Route("GetBudget")]
+        //public async Task<List<Budget>> GetBudget(int hhId)
+        //{
+        //    return await db.GetBudget(hhId);
+        //}
+
+
+        [Route("GetBudgetGoal")]
+        public async Task<IHttpActionResult> GetBudgetGoalsBalanceJson(int budgId)
+        {
+            var json = JsonConvert.SerializeObject(await db.GetBudgetGoalsBalance(budgId));
+            return Ok(json);
+        }
+
+
+
+
+
+
+
+
+
+
+
+        //    [Route("AccountsV2")]
+        //    public IHttpActionResult GetAccounts2(int housholdId)
+        //{
+        //    return db.
+        //}
+
+
+
+
+
+
+
+
+
+        [Route("TransactionDetails")]
+        public async Task<List<Transaction>> GetTransactionDetails(int acctId)
+        {
+            return await db.GetTransactionDetails(acctId);
+        }
+
+        [Route("TransactionsByHouse")]
+        public async Task<List<Transaction>> GetTransactionsByHouse(int hhId)
+        {
+            return await db.GetTransactionDetails(hhId);
+        }
+
+
+        [Route("AddTransaction")]
+        [AcceptVerbs("GET", "POST")]
+        public IHttpActionResult AddTransaction(int accountId, string description, DateTimeOffset date, decimal amount, bool trxType, bool isVoid, int categoryId, string userId, bool reconciled, decimal recBalance, bool isDeleted, DateTimeOffset created)
+        {
+            return Ok(db.AddTransaction(accountId, description, date, amount, trxType, isVoid, categoryId, userId, reconciled, recBalance, isDeleted, created));
+
+        }
+
+
+    }
+
+
+
+}
+
--- HouseFinanceAPI/Models/IdentityModels.cs
+++ HouseFinanceAPI/Models/IdentityModels.cs
@@ -1,8 +1,12 @@
-﻿using System.Security.Claims;
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Security.Claims;
 using System.Threading.Tasks;
 using Microsoft.AspNet.Identity;
 using Microsoft.AspNet.Identity.EntityFramework;
 using Microsoft.AspNet.Identity.Owin;
+using static HouseFinanceAPI.Models.FinanceAPI;
 
 namespace HouseFinanceAPI.Models
 {
@@ -24,10 +28,138 @@ namespace HouseFinanceAPI.Models
             : base("DefaultConnection", throwIfV1Schema: false)
         {
         }
-        
+
         public static ApplicationDbContext Create()
         {
             return new ApplicationDbContext();
         }
+
+
+
+        //SQL Get Accounts
+        public async Task<List<PersonalAccount>> GetAccounts(int HouseholdId)
+        {
+            return await Database.SqlQuery<PersonalAccount>("GetAccount @HouseholdId", 
+                new SqlParameter("HouseholdId", HouseholdId)).ToListAsync();
+        }
+
+        public async Task<PersonalAccount> GetAccountDetails(int accountId, int hhId)
+        {
+            return await Database.SqlQuery<PersonalAccount>("GetAccountDetails @acctId, @hhId",
+                new SqlParameter("acctId", accountId),
+                new SqlParameter("hhId", hhId)).FirstOrDefaultAsync();
+        }
+
+        public async Task<PersonalAccount> GetAccountsBalance( int hhId)
+        {
+            return await Database.SqlQuery<PersonalAccount>("GetAccountsBalance @hhId",              
+                new SqlParameter("hhId", hhId)).FirstOrDefaultAsync();
+        }
+
+        public async Task<PersonalAccount> GetAccountsByHousehold(string Household)
+        {
+            //need to fix my stored procedure, getting all accounts not accounts for the household
+            return await Database.SqlQuery<PersonalAccount>("GetAccountsByHousehold @Household",
+                new SqlParameter("Household", Household)).FirstOrDefaultAsync();
+        }
+
+        public async Task<List<Budget>> GetAllBudgets(int hhId)
+        {
+
+            //This just gets names of budgets, do I really want BudgetItems which has amounts?
+            return await Database.SqlQuery<Budget>("GetAllBudgets @hhId",
+                new SqlParameter("hhId", hhId))./*FirstOrDefaultAsync();*/ToListAsync();
+        }
+
+        public async Task<Budget> GetBudget(int hhId)
+        {
+            return await Database.SqlQuery<Budget>("GetBudget@hhId",
+                new SqlParameter("hhId", hhId)).FirstOrDefaultAsync();
+        }
+
+        public async Task<BudgetItem> GetBudgetGoalsBalance(int hhId)
+        {
+
+            //This stored procedure is getting food budgetgoal, I think I want to change it from hhId to BudgetItem Id
+            return await Database.SqlQuery<BudgetItem>("GetBudgetGoalsBalance @hhId",
+                new SqlParameter("hhId", hhId)).FirstOrDefaultAsync();
+        }
+
+        public async Task<Household> GetHousehold(int Id)
+        {
+            return await Database.SqlQuery<Household>("GetHousehold @Id",
+                new SqlParameter("Id", Id)).FirstOrDefaultAsync();
+        }
+
+        public async Task<List<Transaction>> GetTransactionDetails(int acctId)
+        {
+            return await Database.SqlQuery<Transaction>("GetTransactionDetails @acctId",
+                new SqlParameter("acctId", acctId)).ToListAsync();
+        }
+
+        public async Task<List<Transaction>> GetTransactionsByHouse(int hhId)
+        {
+            return await Database.SqlQuery<Transaction>("GetTransactions @hhId",
+                new SqlParameter("hhId", hhId)).ToListAsync();
+        }
+
+
+
+        //SQL Add Transaction
+        public async Task<int> AddTransaction(int accountId, string description, DateTimeOffset date, decimal amount, bool trxType, bool isVoid, int categoryId, string userId, bool reconciled, decimal recBalance, bool isDeleted, DateTimeOffset created)
+        {
+            return await Database.ExecuteSqlCommandAsync("AddTransaction @acctId, @desc, @date, @amnt, @type, @isVoid, @caty, @user, @reconciled, @recBalance, @deleted", @created,
+                new SqlParameter("acctId", accountId),
+                new SqlParameter("desc", description),
+                new SqlParameter("date", date),
+                new SqlParameter("amnt", amount),
+                new SqlParameter("type", trxType),
+                new SqlParameter("isVoid", isVoid),
+                new SqlParameter("caty", categoryId),
+                new SqlParameter("user", userId),
+                new SqlParameter("reconciled", reconciled),
+                new SqlParameter("recBalance", recBalance),
+                new SqlParameter("deleted", isDeleted),
+                new SqlParameter("created", created));
+        }
+
+        //SQL Add Account
+        public int AddAccount(int hhId, string desc, decimal balance, decimal recBalance, string user, bool deleted)
+        {
+            return Database.ExecuteSqlCommand("AddTransaction @hhId, @desc, @balance, @recBalance, @user, @deleted",
+                new SqlParameter("hhId", hhId),
+                new SqlParameter("desc", desc),
+                new SqlParameter("balance", balance),
+                new SqlParameter("recBalance", recBalance),
+                new SqlParameter("user", user),
+                new SqlParameter("deleted", deleted));
+        }
+
+        //SQL Add BudgetGoal
+        public int AddBudgetGoal(int hhId, string name)
+        {
+            return Database.ExecuteSqlCommand("AddBudgetItem @hhId, @name",
+                new SqlParameter("hhId", hhId),               
+                new SqlParameter("name", name));
+        }
+
+        //SQL Add BudgetItem
+        public int AddBudgetItem(int caty, int budgId, decimal amnt)
+        {
+            return Database.ExecuteSqlCommand("AddBudgetItem @caty, @budgId, @amnt",
+                new SqlParameter("caty", caty),
+                new SqlParameter("budgId", budgId),               
+                new SqlParameter("amnt", amnt));
+        }
+
+        //SQL Create Household(Add new house)
+        public int CreatHousehold(string user)
+        {
+            return Database.ExecuteSqlCommand("CreateHousehold @user",
+               
+                new SqlParameter("user", user));
+        }
+
+
     }
 }
